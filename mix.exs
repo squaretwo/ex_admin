df85ab05b913552555eac80cb5eb316848b82c5b
Donald Plummer <donald@squaretwo.co>
2018-11-01 11:50:11 -0700

    Fix warning

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule ExAdmin.Mixfile do
       deps: deps(),
       package: package(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
       description: """
       An Elixir Phoenix Auto Administration Package.
       """

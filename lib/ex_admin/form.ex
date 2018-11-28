57880f6d39deeab02cafd34e2a0993964829ea4b
Skyler Parr <skylerparr@gmail.com>
2018-11-28 10:29:47 -0500

    added more form types for dates

--- lib/ex_admin/form.ex
+++ lib/ex_admin/form.ex
@@ -1201,12 +1201,39 @@ defmodule ExAdmin.Form do
     |> datetime_select(field_name, Map.get(opts, :options, []))
   end
 
+  def build_control(DateTime, resource, opts, model_name, field_name, _ext_name) do
+    %{name: model_name, model: resource, id: model_name, class: "form-control"}
+    |> datetime_select(field_name, Map.get(opts, :options, []))
+  end
+
+  def build_control(NaiveDateTime, resource, opts, model_name, field_name, _ext_name) do
+    %{name: model_name, model: resource, id: model_name, class: "form-control"}
+    |> datetime_select(field_name, Map.get(opts, :options, []))
+  end
+
   def build_control(Ecto.Date, resource, opts, model_name, field_name, _ext_name) do
     %{name: model_name, model: resource, id: model_name, class: "form-control"}
     |> date_select(field_name, Map.get(opts, :options, []))
   end
 
   def build_control(Ecto.Time, resource, opts, model_name, field_name, _ext_name) do
+    %{name: model_name, model: resource, id: model_name, class: "form-control"}
+    |> time_select(field_name, Map.get(opts, :options, []))
+  end
+
+  def build_control(Elixir.DateTime, resource, opts, model_name, field_name, _ext_name) do
+    %{name: model_name, model: resource, id: model_name, class: "form-control"}
+    |> datetime_select(field_name, Map.get(opts, :options, []))
+  end
+  def build_control(Elixir.NaiveDateTime, resource, opts, model_name, field_name, _ext_name) do
+    %{name: model_name, model: resource, id: model_name, class: "form-control"}
+    |> datetime_select(field_name, Map.get(opts, :options, []))
+  end
+  def build_control(Elixir.Date, resource, opts, model_name, field_name, _ext_name) do
+    %{name: model_name, model: resource, id: model_name, class: "form-control"}
+    |> date_select(field_name, Map.get(opts, :options, []))
+  end
+  def build_control(Elixir.Time, resource, opts, model_name, field_name, _ext_name) do
     %{name: model_name, model: resource, id: model_name, class: "form-control"}
     |> time_select(field_name, Map.get(opts, :options, []))
   end
